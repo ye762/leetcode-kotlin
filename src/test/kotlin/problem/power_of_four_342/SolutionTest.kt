02e3ddb233733f624189bb1d358c160ef5f682cc
ye762 <ksanichalive@gmail.com>
2025-08-16 12:29:21 +0300

    [ye-leetcode-problems] Using concrete assertion's imports.

--- src/test/kotlin/problem/power_of_four_342/SolutionTest.kt
+++ src/test/kotlin/problem/power_of_four_342/SolutionTest.kt
@@ -1,6 +1,7 @@
 package problem.power_of_four_342
 
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Test
 import org.ye.problem.power_of_four_342.Solution
 
